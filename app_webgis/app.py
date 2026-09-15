c631b432af52d7498e5440096d4f115d5ea2371c
Gabriela Rodrigues da Costa <67023156+gabrielacostaa@users.noreply.github.com>
2026-09-15 05:38:33 -0300

    Update app.py

--- app_webgis/app.py
+++ app_webgis/app.py
@@ -100,6 +100,12 @@ def serve_layer(filename):
     elif os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
     abort(404)
+    @app.route('/<path:filename>')
+def serve_direct_geojson(filename):
+    if filename.endswith('.geojson'):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        return send_from_directory(base_dir, filename)
+    abort(404)
 
 @app.route('/upload', methods=['POST'])
 @login_required
